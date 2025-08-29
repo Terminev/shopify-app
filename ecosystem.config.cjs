86c164d3b6b2ae39e9f8bf78f68d346def68b181
Terminev <caramel.alexandre@gmail.com>
2025-08-29 17:28:56 +0200

    Update ecosystem configuration to set PORT to 3000 and change development environment to production for the nodeApp-dev script.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,6 +15,7 @@ module.exports = {
         HOST: "127.0.0.1",
         APP_URL: process.env.APP_URL,
         NODE_ENV: "production",
+        PORT: "3000",
       },
     },
 
@@ -22,15 +23,16 @@ module.exports = {
     {
       name: "nodeApp-dev",
       script: "npm",
-      args: "run dev",
+      args: "run start",
       env: {
         SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY_DEV,
         SHOPIFY_API_SECRET_KEY: process.env.SHOPIFY_API_SECRET_KEY_DEV,
         SHOPIFY_APP_URL: process.env.SHOPIFY_APP_URL_DEV,
         SHOP_CUSTOM_DOMAIN: process.env.SHOP_CUSTOM_DOMAIN_DEV,
         HOST: "127.0.0.1",
+        PORT: "3000",
         APP_URL: process.env.APP_URL_DEV,
-        NODE_ENV: "development",
+        NODE_ENV: "production",
       },
     },
   ],
